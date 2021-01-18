5694eb3a46e2fa294359c682db14085c98843bb9
Ben T <ben.trevethan@gmail.com>
2021-01-18 15:01:17 +1030

    adding data to the users choice of city cards

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-$("#find-city").on("click", function(event) {
-    
-    event.preventDefault();
+$("#find-city").on("click", function(displayWeather) {
+    var localStor = [];
+    displayWeather.preventDefault();
     var city = $("#city-input").val();
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=53df197488a176e2ad8c359a8a8142e4";
-    console.log(queryURL);
-
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=53df197488a176e2ad8c359a8a8142e4";
+    var clicks = 0;
+    clicks++;
     $.ajax({
     url: queryURL,
     }).then(function(response) {
-        console.log(this);
-    console.log(response);
+        $("#city-name1").text(response.name);
+        $("#today-date").text(moment().format('LLLL'));
     });
-
-// -----------------------------------------------------------------------
 
 });
