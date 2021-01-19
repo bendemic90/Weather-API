209c83bb0cbd86c06fb77609ccad7c3fd1a06680
Ben T <ben.trevethan@gmail.com>
2021-01-19 15:54:38 +1030

    updated local time to show city time with unix timestamp offset

--- script.js
+++ script.js
@@ -12,21 +12,23 @@ $("#find-city").on("click", function(displayWeather) {
     if (clicks === 1) {
         console.log(response);
         $("#city-name1").text(response.name);
-        $("#today-date1").text(moment().format('LLLL'));
+        $("#today-date1").text(moment(response.dt, 'X').utcOffset(response.timezone / 3600));
         $("#temperature1").text(response.main.temp);
         $("#humidity1").text(response.main.humidity);
         $("#windspeed1").text(response.wind.speed);
             }
     if (clicks === 2){
+        console.log(response);
         $("#city-name2").text(response.name);
-        $("#today-date2").text(moment().format('LLLL'));
+        $("#today-date2").text(moment(response.dt, 'X').utcOffset(response.timezone / 3600));
         $("#temperature2").text(response.main.temp);
         $("#humidity2").text(response.main.humidity);
         $("#windspeed2").text(response.wind.speed);
     }
     if (clicks === 3){
+        console.log(response);
         $("#city-name3").text(response.name);
-        $("#today-date3").text(moment().format('LLLL'));
+        $("#today-date3").text(moment(response.dt, 'X').utcOffset(response.timezone / 3600));
         $("#temperature3").text(response.main.temp);
         $("#humidity3").text(response.main.humidity);
         $("#windspeed3").text(response.wind.speed);
