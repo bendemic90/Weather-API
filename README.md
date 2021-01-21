5196520d00a9facebda7560b9285ecb9ab5d9d69
Ben T <ben.trevethan@gmail.com>
2021-01-21 16:40:09 +1030

    updating readme and adding 5day functionality to all 3 cards

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Weather-API
-A simple weather app.
+A simple weather app that displays current and future weather conditions.
+
+## Features
+* Has a start button that loads the quiz, selecting random questions.
+* Correct answers should have increased your score by 1, while incorrect would deduct time from the timer. I didn't get this far though.
+* Once a question is answered it SHOULD move you onto the next question, although this is not the case.
+
+
+## Reasoning
+Utilisation of randomisation and local storage, hiding elements and keeping it all together.
+
+## Challenges & Process
+1. I found the layout process pretty easy, I used the Captain Planet activity and bootstrapped the layout from that.
+2. There's a lot of redundant code but I was running out of time to compact it all, resulting in lots of repitition and hundreds more lines than is necessary.
+3. The most challenging part was the decision to upload it as an assignment OR not upload anything at all.
+4. Definitely didn't leave myself enough time over the holidays to complete this.
+
+## Link to deployed application
+https://bendemic90.github.io/Coding-quiz/
+
+## Screengrab of deployed application
+![index.html](https://github.com/bendemic90/Coding-quiz/blob/main/assets/grab.png)
--- script.js
+++ script.js
@@ -26,7 +26,7 @@ $("#find-city").on("click", function(displayWeather) {
         $("#windspeed1").text(response.wind.speed);
             $.ajax({
                 url: forecastURL,
-            }).then(function (forecastResponse){
+                }).then(function (forecastResponse){
                 $("#w1-d2").text(forecastResponse.list[9].main.temp 
                     + ", " 
                     + forecastResponse.list[9].weather[0].main
@@ -49,20 +49,68 @@ $("#find-city").on("click", function(displayWeather) {
                     + forecastResponse.list[33].dt_txt);
             })
             }
-    if (clicks === 2){
+    if (clicks === 2) {
         $("#city-name2").text(response.name);
         $("#today-date2").text(moment(response.dt, 'X').utcOffset(response.timezone / 3600));
         $("#temperature2").text(response.main.temp);
         $("#humidity2").text(response.main.humidity);
         $("#windspeed2").text(response.wind.speed);
+            $.ajax({
+            url: forecastURL,
+        }).then(function (forecastResponse){
+            $("#w2-d2").text(forecastResponse.list[9].main.temp 
+                + ", " 
+                + forecastResponse.list[9].weather[0].main
+                + ", "
+                + forecastResponse.list[9].dt_txt);
+            $("#w2-d3").text(forecastResponse.list[17].main.temp
+                + ", " 
+                + forecastResponse.list[17].weather[0].main
+                + ", "
+                + forecastResponse.list[17].dt_txt);
+            $("#w2-d4").text(forecastResponse.list[25].main.temp
+                + ", " 
+                + forecastResponse.list[25].weather[0].main
+                + ", "
+                + forecastResponse.list[25].dt_txt);
+            $("#w2-d5").text(forecastResponse.list[33].main.temp
+                + ", " 
+                + forecastResponse.list[33].weather[0].main
+                + ", "
+                + forecastResponse.list[33].dt_txt);
+        })
     }
-    if (clicks === 3){
+    if (clicks === 3) {
         $("#city-name3").text(response.name);
         $("#today-date3").text(moment(response.dt, 'X').utcOffset(response.timezone / 3600));
         $("#temperature3").text(response.main.temp);
         $("#humidity3").text(response.main.humidity);
         $("#windspeed3").text(response.wind.speed);
         clicks = 0
+        $.ajax({
+            url: forecastURL,
+        }).then(function (forecastResponse){
+            $("#w3-d2").text(forecastResponse.list[9].main.temp 
+                + ", " 
+                + forecastResponse.list[9].weather[0].main
+                + ", "
+                + forecastResponse.list[9].dt_txt);
+            $("#w3-d3").text(forecastResponse.list[17].main.temp
+                + ", " 
+                + forecastResponse.list[17].weather[0].main
+                + ", "
+                + forecastResponse.list[17].dt_txt);
+            $("#w3-d4").text(forecastResponse.list[25].main.temp
+                + ", " 
+                + forecastResponse.list[25].weather[0].main
+                + ", "
+                + forecastResponse.list[25].dt_txt);
+            $("#w3-d5").text(forecastResponse.list[33].main.temp
+                + ", " 
+                + forecastResponse.list[33].weather[0].main
+                + ", "
+                + forecastResponse.list[33].dt_txt);
+        })
     }
     });
 
